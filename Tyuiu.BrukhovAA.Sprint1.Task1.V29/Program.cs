aa0ea5aac368421ecff9197a656423fcee0e7577
Tyuiu.BrukhovAA@outlook.com <bruhov_uch@mail.ru>
2023-09-19 21:45:40 +0500

    Task1

---
+++ Tyuiu.BrukhovAA.Sprint1.Task1.V29/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.BrukhovAA.Sprint1.Task1.V29.Lib;
+
+
+namespace Tyuiu.BrukhovAA.Sprint1.Task1.V29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Брюхов А. А. | АСОиУБ-23-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #1                                                              *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                       *");
+            Console.WriteLine("* Задание #1                                                             *");
+            Console.WriteLine("* Вариант #29                                                            *");
+            Console.WriteLine("* Выполнил: Брюхов Алексей Андреевич | АСОиУБ-23-1                       *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные,*");
+            Console.WriteLine("* вычисляет результат по формуле a*b*c/(a+b+c) и печатает его на экране  *");
+            Console.WriteLine("*                                                                        *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            double a, b, c;
+
+            Console.WriteLine("Введите значение a");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение b");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение c");
+            c = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine(ds.Calculate(a, b, c));
+
+            Console.ReadLine();
+        }
+    }
+}
