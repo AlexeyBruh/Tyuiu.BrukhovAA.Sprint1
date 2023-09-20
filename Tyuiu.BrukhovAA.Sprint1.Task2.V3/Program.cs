afe153140ff6c172c346c6c69b331bd0f03e7adf
Tyuiu.BrukhovAA@outlook.com <bruhov_uch@mail.ru>
2023-09-20 21:26:34 +0500

    Task2

--- Tyuiu.BrukhovAA.Sprint1.Task2.V3/Program.cs
+++ Tyuiu.BrukhovAA.Sprint1.Task2.V3/Program.cs
@@ -4,31 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Tyuiu.BrukhovAA.Sprint1.Task2.V3.Lib;
+
 namespace Tyuiu.BrukhovAA.Sprint1.Task2.V3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.Title = "Спринт #0 | Выполнил: Брюхов А. А. | АСОиУБ-23-1";
+            Console.Title = "Спринт #1 | Выполнил: Брюхов А. А. | АСОиУБ-23-1";
             Console.WriteLine("**************************************************************************");
-            Console.WriteLine("* Спринт #0                                                              *");
-            Console.WriteLine("* Тема: Создание итогового решения по спринту                            *");
-            Console.WriteLine("* Задание #7                                                             *");
-            Console.WriteLine("* Вариант #0                                                             *");
+            Console.WriteLine("* Спринт #1                                                              *");
+            Console.WriteLine("* Тема: Арифметические операторы в C#                                    *");
+            Console.WriteLine("* Задание #2                                                             *");
+            Console.WriteLine("* Вариант #3                                                             *");
             Console.WriteLine("* Выполнил: Брюхов Алексей Андреевич | АСОиУБ-23-1                       *");
             Console.WriteLine("**************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                               *");
-            Console.WriteLine("* Написать консольную программу на C#, которая суммирует значения двух   *");
-            Console.WriteLine("* Одинаковых массивов по длинне.                                         *");
+            Console.WriteLine("* Задано количество часов.                                               *");
+            Console.WriteLine("* Перевести время в минуты.                                              *");
             Console.WriteLine("*                                                                        *");
             Console.WriteLine("**************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
             Console.WriteLine("**************************************************************************");
-            Console.WriteLine("*                                                                        *");
+
+            DataService ds = new DataService();
+            int x;
+
+            Console.WriteLine("Введите время в часах");
+            x = Convert.ToInt32(Console.ReadLine());
+
             Console.WriteLine("**************************************************************************");
-            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
             Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine("Время в минутах:" + ds.ConvertHourToMin(x));
+
+            Console.ReadLine();
         }
     }
 }
