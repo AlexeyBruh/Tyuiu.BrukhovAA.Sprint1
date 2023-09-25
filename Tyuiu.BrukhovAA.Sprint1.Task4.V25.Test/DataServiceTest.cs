62e86af45eee71a4c4f49043dbcb82c6b43b2506
Tyuiu.BrukhovAA@outlook.com <bruhov_uch@mail.ru>
2023-09-25 14:45:30 +0500

    Task4

---
+++ Tyuiu.BrukhovAA.Sprint1.Task4.V25.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.BrukhovAA.Sprint1.Task4.V25.Lib;
+
+namespace Tyuiu.BrukhovAA.Sprint1.Task4.V25.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double a = 30;
+            double wait = Math.Round(0.866362, 3);
+            var res = ds.Calculate(a);
+            Assert.AreEqual(wait, Math.Round(res, 3));
+        }
+    }
+}
---
+++ Tyuiu.BrukhovAA.Sprint1.Task4.V25.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BrukhovAA.Sprint1.Task4.V25.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BrukhovAA.Sprint1.Task4.V25.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1652488c-9046-496f-9edb-15433a65e4d9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
